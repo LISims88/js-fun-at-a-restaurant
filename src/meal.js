6a65d1ee707a7c0872aaaeebed9d6152c93233fe
Lydia Sims <sims.lydia88@gmail.com>
2023-11-30 21:02:43 -0600

    Completed sections and Tests

--- src/meal.js
+++ src/meal.js
@@ -1,11 +1,24 @@
-function nameMenuItem() {
+function nameMenuItem (food){
+  // write a function that interpolates a
+  // food item with the string 'Delicous [food]'
+  return `Delicious ${food}`
+} 
 
+function createMenuItem(){
+  //create a function
+var nameMenuItem = {
+  name: "French Toast",
+  price: 10.99
+  type: "breakfast"
+}//create an object defined as "French Toast",
+  // that is priced at 10.99 and is a breakfast
+  // item
+  
 }
-
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
