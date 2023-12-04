c855c5e83248fbb448a23c526d82cc29646e7f84
Lydia Sims <sims.lydia88@gmail.com>
2023-12-03 18:55:25 -0600

    worked on takeOrder and refundOrder

--- src/order.js
+++ src/order.js
@@ -1,20 +1,41 @@
-// Function should add new orders in an order type list with specific
-// parameters printing out of the order and the order pickup type
-function takeOrder() {
+// this means that the function of take order has to have two parameters whether the delivery  and status.
+// these items must be listed within the object as well as the order number, what the item is, the item's price and th
+
+function takeOrder(orderNumber, foodItem, itemPrice, restaurantStatus, deliveryOrders) {
+  // Function should add new orders in an order type list with specific parameters printing out of the order and the order pickup type
+
+  //the var of order must be an object that can be added to an array this means that a method of .includes and .push are required to return
+  //the array list should have not be able to hold more than 3 orders at a time 
   var order = {
     number: orderNumber,
     item: foodItem,
     price: itemPrice,
-    orderType: pickupType,
+    orderType: "delivery",
     orderStatus: restaurantStatus,
   };
-    
+   var pickupType = order.orderType
+
+  if (deliveryOrders.includes(pickupType)) {
+    deliveryOrders.push(pickupType)
+  }
+else {
+  Array.length = 2
+}
+   console.log("New order",order)
+    console.log("Order pickup type", pickupType)
+    return order 
+
+    //I unfortunately am not having success in doing so
 }
 
 //This function should be able to remove an order by the number and reduce
 //the number of by one when prompted
-function refundOrder () {
 
+function refundOrder (orderNumber,deliveryOrders) {
+if (orderNumber = deliveryOrders){
+  deliveryOrder.splice(order.indexOf(orderNumber))
+}
+ return refundOrder
 } 
 
 //this function should be able to list the orders by name and possible group
